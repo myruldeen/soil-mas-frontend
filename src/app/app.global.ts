379ffb495aab1b99fc9c3edc6fe7245891b26f92
myruldeen <funstudy10@gmail.com>
2023-07-05 11:29:31 +0000

    update icon

--- src/app/app.global.ts
+++ src/app/app.global.ts
@@ -1,6 +1,6 @@
 // http://stackoverflow.com/a/39360585/1015046
 export const Globals = Object.freeze({
-	BASE_API_URL: 'http://localhost:9000/',
+	BASE_API_URL: 'http://srv-deno-1:7000/',
 	API_AUTH_TOKEN: 'AUTH_TOKEN',
 	AUTH_USER: 'AUTH_USER'
 });
